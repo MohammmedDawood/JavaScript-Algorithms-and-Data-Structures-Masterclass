e14214bbeecf53d4efbe523ece53bb0013a32289
Mohammed Dawood <mahmedtony73@gmail.com>
2022-09-30 22:40:05 +0200

    Add up to

---
+++ add-up-to.js
@@ -0,0 +1,21 @@
+// add up to n slowly
+// function addUpTo(n) {
+//   let total = 0;
+//   for (let i = 1; i <= n; i++) {
+//     total += i;
+//   }
+//   return total;
+// }
+
+// add up to n quickly
+function addUpTo(n) {
+  return (n * (n + 1)) / 2;
+}
+
+console.log(addUpTo(6));
+
+// performance of the function
+let t1 = performance.now();
+addUpTo(1000000000);
+let t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
