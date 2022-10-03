f91704a8f5dde918c28aa9420f51a754702450c3
Mohammed Dawood <mahmedtony73@gmail.com>
2022-10-03 08:21:09 +0200

    refactor

--- add-up-to.js
+++ add-up-to.js
@@ -1,5 +1,5 @@
+// check this link for more information about the function timer perormance tracker
 // https://rithmschool.github.io/function-timer-demo/
-// check this link for more information
 
 // add up to n slowly perform 5n+2 operations O(n)
 // function addUpTo(n) {
