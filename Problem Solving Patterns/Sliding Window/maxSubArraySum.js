2814c468e87179fd05da6152403bbcd7310f5a7f
Mohammed Dawood <mahmedtony73@gmail.com>
2022-10-15 23:02:34 +0200

    add sliding window solution

--- Problem Solving Patterns/Sliding Window/maxSubArraySum.js
+++ Problem Solving Patterns/Sliding Window/maxSubArraySum.js
@@ -6,27 +6,46 @@
 // Naive Solution
 // Time Complexity - O(n^2)
 // Space Complexity - O(1)
+// function maxSubarraySum(arr, num) {
+//   if (num > arr.length) {
+//     return null;
+//   }
+//   let max = -Infinity;
+//   for (let i = 0; i < arr.length - num + 1; i++) {
+//     temp = 0;
+//     for (let j = 0; j < num; j++) {
+//       temp += arr[i + j];
+//     }
+//     if (temp > max) {
+//       max = temp;
+//     }
+//     // console.log(temp);
+//     // console.log(max);
+//   }
+//   return max;
+// }
+
+// Refactored Solution - Sliding Window
+// Time Complexity - O(n)
+// Space Complexity - O(1)
 function maxSubarraySum(arr, num) {
-  if (num > arr.length) {
-    return null;
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
   }
-  let max = -Infinity;
-  for (let i = 0; i < arr.length - num + 1; i++) {
-    temp = 0;
-    for (let j = 0; j < num; j++) {
-      temp += arr[i + j];
-    }
-    if (temp > max) {
-      max = temp;
-    }
-    // console.log(temp);
-    // console.log(max);
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
   }
-  return max;
+
+  return maxSum;
 }
 
-console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)); // 10
+// console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)); // 10
 console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4)); // 17
-console.log(maxSubarraySum([4, 2, 1, 6], 1)); // 6
-console.log(maxSubarraySum([4, 2, 1, 6, 2], 4)); // 13
-console.log(maxSubarraySum([], 4)); // null
+// console.log(maxSubarraySum([4, 2, 1, 6], 1)); // 6
+// console.log(maxSubarraySum([4, 2, 1, 6, 2], 4)); // 13
+// console.log(maxSubarraySum([], 4)); // null
