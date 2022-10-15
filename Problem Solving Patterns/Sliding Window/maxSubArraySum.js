92543015298322d25f51d34043c32fa78d695354
Mohammed Dawood <mahmedtony73@gmail.com>
2022-10-15 22:44:45 +0200

    Create maxSubArraySum.js

---
+++ Problem Solving Patterns/Sliding Window/maxSubArraySum.js
@@ -0,0 +1,32 @@
+// Write a function called maxSubarraySum which accepts
+// an array of integers and a number called n. The function
+// should calculate the maximum sum of n consecutive
+// elements in the array.
+
+// Naive Solution
+// Time Complexity - O(n^2)
+// Space Complexity - O(1)
+function maxSubarraySum(arr, num) {
+  if (num > arr.length) {
+    return null;
+  }
+  let max = -Infinity;
+  for (let i = 0; i < arr.length - num + 1; i++) {
+    temp = 0;
+    for (let j = 0; j < num; j++) {
+      temp += arr[i + j];
+    }
+    if (temp > max) {
+      max = temp;
+    }
+    // console.log(temp);
+    // console.log(max);
+  }
+  return max;
+}
+
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)); // 10
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4)); // 17
+console.log(maxSubarraySum([4, 2, 1, 6], 1)); // 6
+console.log(maxSubarraySum([4, 2, 1, 6, 2], 4)); // 13
+console.log(maxSubarraySum([], 4)); // null
