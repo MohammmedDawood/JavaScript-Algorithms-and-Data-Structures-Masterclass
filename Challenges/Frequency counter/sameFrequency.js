62277fd886e8c2b46062bc31307034b743e59736
Mohammed Dawood <mahmedtony73@gmail.com>
2022-10-29 22:51:45 +0200

    Update sameFrequency.js

--- Challenges/Frequency counter/sameFrequency.js
+++ Challenges/Frequency counter/sameFrequency.js
@@ -1,3 +1,4 @@
+// Frequency Counter - sameFrequency
 // Write a function called sameFrequency. Given two positive integers,
 // find out if the two numbers have the same frequency of digits.
 // Your solution MUST have the following complexities:
