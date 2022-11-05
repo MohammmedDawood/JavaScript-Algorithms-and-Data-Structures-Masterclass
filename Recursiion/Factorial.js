d7a84376df6277d364a97195b4ca18329c88fd35
Mohammed Dawood <mahmedtony73@gmail.com>
2022-11-05 22:10:33 +0200

    Update Factorial.js
    common problems with recursion

--- Recursiion/Factorial.js
+++ Recursiion/Factorial.js
@@ -9,3 +9,8 @@ console.log(Factorial(3)); // 6
 console.log(Factorial(4)); // 24
 console.log(Factorial(5)); // 120
 console.log(Factorial(6)); // 720
+
+// common problems with recursion
+// pitfall: is base case isn't correct, you'll get a stack overflow error
+// it fails is to forget to return the result of the recursive call
+// stack overflow error
