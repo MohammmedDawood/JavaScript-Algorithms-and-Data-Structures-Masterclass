87728992b123847370d5d6bbceac115d601e0363
Mohammed Dawood <mahmedtony73@gmail.com>
2022-11-13 08:22:44 +0200

    Update flatten.js

--- Bonus CHALLENGING Recursion Problems/flatten.js
+++ Bonus CHALLENGING Recursion Problems/flatten.js
@@ -2,6 +2,13 @@
 // Write a recursive function called flatten which accepts an
 // array of arrays and returns a new array with all values flattened.
 
+// function flatten(arr) {
+//   if (arr.length === 0) return [];
+//   if (Array.isArray(arr[0]))
+//     return flatten(arr[0]).concat(flatten(arr.slice(1)));
+//   return [arr[0]].concat(flatten(arr.slice(1)));
+// }
+
 function flatten(oldArr) {
   var newArr = [];
   for (var i = 0; i < oldArr.length; i++) {
