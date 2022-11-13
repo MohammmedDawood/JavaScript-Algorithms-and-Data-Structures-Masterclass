9af01324d86699549458cc8d49f82c65e04dd79f
Mohammed Dawood <mahmedtony73@gmail.com>
2022-11-13 09:11:57 +0200

    Update capitalizeWords.js

--- Bonus CHALLENGING Recursion Problems/capitalizeWords.js
+++ Bonus CHALLENGING Recursion Problems/capitalizeWords.js
@@ -2,9 +2,20 @@
 // Write a recursive function called capitalizeWords.
 // Given an array of words, return a new array containing each word capitalized.
 
-function capitalizeWords(arr) {
-  if (arr.length === 0) return [];
-  return [arr[0].toUpperCase()].concat(capitalizeWords(arr.slice(1)));
+// function capitalizeWords(arr) {
+//   if (arr.length === 0) return [];
+//   return [arr[0].toUpperCase()].concat(capitalizeWords(arr.slice(1)));
+// }
+
+//solution
+
+function capitalizeWords(array) {
+  if (array.length === 1) {
+    return [array[0].toUpperCase()];
+  }
+  let res = capitalizeWords(array.slice(0, -1));
+  res.push(array.slice(array.length - 1)[0].toUpperCase());
+  return res;
 }
 
 let words = ["i", "am", "learning", "recursion"];
