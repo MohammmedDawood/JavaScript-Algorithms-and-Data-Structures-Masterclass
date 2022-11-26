88788e9d4c01fc9f20bec096a4b399695990caef
Mohammed Dawood <mahmedtony73@gmail.com>
2022-11-26 22:15:30 +0200

    refactor

--- Searching Algorithms/binarySearch.js
+++ Searching Algorithms/binarySearch.js
@@ -7,18 +7,11 @@ function binarySearch(arr, val) {
   let end = arr.length - 1;
   let middle = Math.floor((start + end) / 2);
   while (arr[middle] !== val && start <= end) {
-    if (val < arr[middle]) {
-      end = middle - 1;
-    } else {
-      start = middle + 1;
-    }
+    if (val < arr[middle]) end = middle - 1;
+    else start = middle + 1;
     middle = Math.floor((start + end) / 2);
   }
-
-  if (arr[middle] === val) {
-    return middle;
-  }
-  return -1;
+  arr[middle] === val ? middle : -1;
 }
 
 console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 15)); // -1
