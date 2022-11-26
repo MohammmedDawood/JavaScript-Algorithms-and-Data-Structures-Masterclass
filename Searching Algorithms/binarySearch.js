5173d9de56de35ddc81699c5a1722e361e92c8dd
Mohammed Dawood <mahmedtony73@gmail.com>
2022-11-26 21:49:31 +0200

    Create binarySearch.js

---
+++ Searching Algorithms/binarySearch.js
@@ -0,0 +1,29 @@
+// Write a function called binarySearch which accepts a
+// sorted array and a value and returns the index at which the value exists.
+// Otherwise, return
+
+function binarySearch(arr, val) {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+  while (arr[middle] !== val && start <= end) {
+    if (val < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+    middle = Math.floor((start + end) / 2);
+  }
+  if (arr[middle] === val) {
+    return middle;
+  }
+  return -1;
+}
+
+console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 103)); // -1
+console.log(binarySearch([1, 2, 3, 4, 5], 2)); // 1
+console.log(binarySearch([1, 2, 3, 4, 5], 3)); // 2
+console.log(binarySearch([1, 2, 3, 4, 5], 5)); // 4
+console.log(binarySearch([1, 2, 3, 4, 5], 6)); // -1
+console.log(binarySearch([1, 2, 3, 4, 5], 1)); // 0
+console.log(binarySearch([1, 2, 3, 4, 5], 4)); // 3
